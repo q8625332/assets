33313efdc6ab6af09cad7af7773a6b95c8ccfb65
ljq <867170960@qq.com>
2019-02-16 10:30:46 +0800

    add assets

---
+++ src/main/java/com/ljq/assets/AssetsApplication.java
@@ -0,0 +1,14 @@
+package com.ljq.assets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsApplication.class, args);
+	}
+
+}
+
